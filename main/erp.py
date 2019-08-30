8789789de17e728081472fd959b5be28de3d468e
Oksana <32892779+Sombercy@users.noreply.github.com>
2019-08-30 18:48:51 +0300

    Update erp.py
    adding specific feature selection

--- main/erp.py
+++ main/erp.py
@@ -5,14 +5,32 @@ import computations as comp
 import functional as func
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
+import feature_selection as fs
 
 
-erps = pd.read_csv('ERPdata.csv', index_col = 'subject')
+data = pd.read_csv('ERPdata.csv', index_col = 'subject')
 #data = pd.read_csv('mergedTrialData.csv', index_col = 'subject')
 demo = pd.read_csv('demographic.csv', index_col = 'subject')
+time = pd.read_csv('time.csv')
+"""t, HC, SZ = map(np.array, comp.pwr_time(erps, demo, 1, 0, 300, export = True))
+y = SZ
+time = pd.read_csv('time.csv')
+data = data[data['condition'] == 1].drop('condition',axis = 1)  
+time = time[:][time['time_ms'] >= 0]
+time = time[:][time['time_ms'] <= 300]
+data = data[data['time_ms'] <= 300]
+data = data[data['time_ms'] >= 0].drop('time_ms', axis = 1)"""
 
-"""t, HC, SZ = map(np.array, comp.pwr_time(erps, demo, 1, -100, 300, export = True))
-y = HC
+""""temp = data.loc[5]
+
+temp.index = range(temp.shape[0])
+temp = temp**2
+temp = temp.sum(axis = 1, skipna = True)
+temp = np.sqrt(temp)
+temp = [temp[x]*100/max(temp) for x in range(len(temp))]
+fig, ax = plt.subplots(figsize=(10, 7), facecolor = 'w')
+ax.plot(time['time_ms'], temp)
+y = np.array(temp)
 M = len(y.tolist())
 fe = 1
 feh = 2*np.pi/M
@@ -23,34 +41,55 @@ fig, ax = plt.subplots(figsize=(10, 7), facecolor = 'w')
 ax.plot(t[:], yscsa[:] + 30, label = 'Right')
 ax.plot(t[:], y[:])
 ax.plot(t[:], y-yscsa - 30)
-plt.show()
+ax.plot(t, psinnor)
 kappa = np.array(kappa)
-print(kappa.diagonal())"""
-
-t, X, Y = comp.dataset(erps, demo, 1, 0, 300)
+kappa = kappa.diagonal())
+print(Nh)
+#print(kappa.diagonal())"""
+tmin = 0
+tmax = 300
+time = time[time['time_ms'] >= tmin]
+time = time[time['time_ms'] <= tmax]
+time = time['time_ms']
+time.index = range(time.shape[0])
+timepoints = len(time)
+X, Y = comp.dataset(data, demo, 1, tmin, tmax, timepoints)
 #subs = list(set(data.index.values.tolist()))
 #subs = [i for i in subs if (list(demo.loc[i])[0] == 0)]
 lamda = []
 Nh = []
-for i in range(len(Y)):
+psi = [] 
+size = len(Y)
+
+for i in range(size):
     #print(X[i])
-    kappa, n = comp.eigen(np.array(X[i]))
+    kappa, n, psinnor = comp.eigen(np.array(X[i]))
     lamda.append(kappa.diagonal())
     Nh.append(n)
-""" Ind = np.array([1 if X[i][j]<0 else 0 for j in range(len(X[i]))])
-    y = X[i] - Ind*min(X[i])"""
-         
-Nh = min(Nh)
-print(Nh)
+    psi.append(psinnor)
+# Ind = np.array([1 if X[i][j]<0 else 0 for j in range(len(X[i]))])
+#    y = X[i] - Ind*min(X[i])
 
+"""set1 = np.array([fs.t_comp(time, lamda[i], psi[i]) for i in range(size)])
+set2 = np.array([fs.ngreates(31, lamda[i], psi[i]) for i in range(size)])
+eigenvals = [lamda[i][set1[i]].tolist() for i in range(size)]
+time_points = [time.loc[set2[i]].values.tolist() for i in range(size)]
+cols1 = ['eigenvalue_%s' % x for x in range(set1.shape[1])]
+cols2 = ['feature_%s' % x for x in range(set2.shape[1])]
+dataset1 = pd.DataFrame(np.array(eigenvals).tolist(),columns = np.array(cols1))
+dataset2 = pd.DataFrame(np.array(time_points).tolist(), columns = np.array(cols2))   
+dataset1['Type'] = np.array(Y)   
+dataset2['Type'] = np.array(Y)   """
+Nh = min(Nh)
+#print(Nh)
 features = np.array([x[:Nh] for x in lamda])
 cols = ['eigenvalue_%s' % x for x in range(Nh)]
 data = pd.DataFrame(features, columns = cols)
 data['Type'] = np.array(Y)
-#data = data.sample(frac=1).reset_index(drop=True)
+#data = data.sample(frac=1).reset_index(drop=True)"""
 X_train, X_test, y_train, y_test = train_test_split(
-        data[cols], data['Type'], test_size = 0.2)
-neigh = KNeighborsClassifier(n_neighbors=5, weights = 'distance')
-neigh.fit(X_train, y_train) 
+        data[cols], data['Type'], test_size = 0.25, random_state = 42)
+neigh = KNeighborsClassifier(n_neighbors=3)
+neigh.fit(X_train, y_train)
 print(neigh.score(X_test, y_test))
-#comp.rl_plt(erps, demo, 1, 1)
+#comp.rl_plt(erps, demo, 1, 1)"""
